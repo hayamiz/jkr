7f802e3d631df5ab0779b485b6f1a097f96298aa
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-10-12 21:57:09 +0900

    [spec] add fixture_path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
 # encoding: utf-8
 
-require 'rubygems'
-
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
+FIXTURE_DIR = File.expand_path('../fixtures', __FILE__)
 
+require 'rubygems'
 require 'jkr'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  def fixture_path(basename)
+    File.expand_path(basename, FIXTURE_DIR)
+  end
+end
 
